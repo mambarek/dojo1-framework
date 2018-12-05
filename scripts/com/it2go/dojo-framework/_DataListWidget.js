3ae3b6172086c12189bac0698c1cb902127b9149
Mbarek, Mohamed Ali <mbarek@kabelmail.de>
2018-12-05 22:48:22 +0100

    generated files from CVS removed

--- scripts/com/it2go/dojo-framework/_DataListWidget.js
+++
@@ -1,4 +0,0 @@
-/*import _DataViewWidget from "./_DataViewWidget"
-
-let w:_DataViewWidget = new _DataViewWidget();*/
-//# sourceMappingURL=_DataListWidget.js.map
--- scripts/test/nls/main.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.root = {
-        dialogTitle: 'Hello World',
-        dialogContent: 'Loaded successfully!'
-    };
-});
-//# sourceMappingURL=main.js.map
